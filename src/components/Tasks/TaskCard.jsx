e79a3af82030eb4c7563288f9b030eaa298dc0ed
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-09-20 21:10:38 +0200

    tasks are now filtered based on the study student is enrolled in

--- src/components/Tasks/TaskCard.jsx
+++ src/components/Tasks/TaskCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default function TaskCard() {
-    return (
-        <div className='task-card'>
-            <h1 className='task-card-title'>{title}</h1>
-            <p className='task-card-text'>{text}</p>
-        </div>
+export default function TaskCard({taskCard}) {
+    const { title, text, subject } = taskCard
+
+return (
+    <div className='task-card'>
+        <h1 className='task-card-title'>{title}</h1>
+        <p className='task-card-text'>{text}</p>
+        <span>{subject}</span>
+    </div>
     )
 }
--- src/taskList.js
+++ src/taskList.js
@@ -1,19 +1,27 @@
 const  taskList = [
     {
         title: "task title1",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quasi mollitia possimus a temporibus ipsam laboriosam in aperiam quos maxime est illo dicta corporis, id quia consectetur magnam, error quisquam totam eum perferendis, quo ea enim? Ipsam iste totam dignissimos laborum quo perferendis recusandae ut, quasi vitae hic nostrum ullam?"
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quasi mollitia possimus a temporibus ipsam laboriosam in aperiam quos maxime est illo dicta corporis, id quia consectetur magnam, error quisquam totam eum perferendis, quo ea enim? Ipsam iste totam dignissimos laborum quo perferendis recusandae ut, quasi vitae hic nostrum ullam?",
+        study: "hospitality",
+        subject: "economics"
     },
     {
         title: "task title1",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quasi mollitia possimus a temporibus ipsam laboriosam in aperiam quos maxime est illo dicta corporis, id quia consectetur magnam, error quisquam totam eum perferendis, quo ea enim? Ipsam iste totam dignissimos laborum quo perferendis recusandae ut, quasi vitae hic nostrum ullam?"
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quasi mollitia possimus a temporibus ipsam laboriosam in aperiam quos maxime est illo dicta corporis, id quia consectetur magnam, error quisquam totam eum perferendis, quo ea enim? Ipsam iste totam dignissimos laborum quo perferendis recusandae ut, quasi vitae hic nostrum ullam?",
+        study: "hospitality",
+        subject: "tourism"
     },
     {
         title: "task title1",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quasi mollitia possimus a temporibus ipsam laboriosam in aperiam quos maxime est illo dicta corporis, id quia consectetur magnam, error quisquam totam eum perferendis, quo ea enim? Ipsam iste totam dignissimos laborum quo perferendis recusandae ut, quasi vitae hic nostrum ullam?"
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quasi mollitia possimus a temporibus ipsam laboriosam in aperiam quos maxime est illo dicta corporis, id quia consectetur magnam, error quisquam totam eum perferendis, quo ea enim? Ipsam iste totam dignissimos laborum quo perferendis recusandae ut, quasi vitae hic nostrum ullam?",
+        study: "mechatronics",
+        subject: "mechanics"
     },
     {
         title: "task title1",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quasi mollitia possimus a temporibus ipsam laboriosam in aperiam quos maxime est illo dicta corporis, id quia consectetur magnam, error quisquam totam eum perferendis, quo ea enim? Ipsam iste totam dignissimos laborum quo perferendis recusandae ut, quasi vitae hic nostrum ullam?"
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quasi mollitia possimus a temporibus ipsam laboriosam in aperiam quos maxime est illo dicta corporis, id quia consectetur magnam, error quisquam totam eum perferendis, quo ea enim? Ipsam iste totam dignissimos laborum quo perferendis recusandae ut, quasi vitae hic nostrum ullam?",
+        study: "mechatronics",
+        subject: "mechanics"
     },
 ]
 
