fc753377dc70e9636de44dd9fe93b78603731931
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-09-22 16:40:01 +0200

    added search to library page

--- src/pages/LibraryPage.jsx
+++ src/pages/LibraryPage.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React, { useState } from "react";
+import Navbar from "../components/Navbar/Navbar";
+import axios from "axios";
 
 export default function LibraryPage() {
-    return (
-        <div id='library'>
-            <Navbar />
-            Library
+  const apiKey = "AIzaSyAXI3E9DPOQNaxMGoStQp2JVaYY5NFQhnI";
+  const [serchText, setSearchText] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=" +
+          serchText +
+          "&key=" +
+          apiKey
+      )
+      .then((res) => {
+        setResults(res.data.items);
+        console.log(results);
+      });
+  };
+
+  const displayBooks = results.map((book) => {
+    return <img src={book.volumeInfo.imageLinks.thumbnail} alt="" />;
+  });
+
+  return (
+    <div id="library">
+      <Navbar />
+      <div className="container">
+        <div className="search-container">
+          <input
+            type="text"
+            value={serchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+          
         </div>
-    )
+        <div className="book-container">{displayBooks}</div>
+      </div>
+    </div>
+  );
 }
