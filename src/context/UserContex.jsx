663059895ad3812995496120cf78f3399e88916a
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-09-25 14:49:08 +0200

    added option to show classes of a chosen semester

--- src/context/UserContex.jsx
+++ src/context/UserContex.jsx
@@ -106,15 +106,6 @@ export const UserContextProvider = ({ children }) => {
           ects: 4
         },
         {
-          title: 'Tourism Geography I',
-          semester: 1,
-          exam1: null,
-          exam2: null,
-          essay: null,
-          presentation: null,
-          ects: 5
-        },
-        {
           title: 'Computer Science in Business II',
           semester: 2,
           exam1: null,
@@ -151,14 +142,212 @@ export const UserContextProvider = ({ children }) => {
           ects: 4
         },
         {
-          title: 'Tourism Geography II',
-          semester: 2,
+          title: 'Business Statistics I',
+          semester: 3,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 3
+        },
+        {
+          title: 'Fundamentals of Accounting I',
+          semester: 3,
           exam1: null,
           exam2: null,
           essay: null,
           presentation: null,
           ects: 4
-        }
+        },
+        {
+          title: 'Fundamentals of Entrepreneurship',
+          semester: 3,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 4
+        },
+        {
+          title: 'Fundamentals of Marketing',
+          semester: 3,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 4
+        },
+        {
+          title: 'Hospitality Management (1/2)',
+          semester: 3,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 5
+        },
+        {
+          title: 'Physical Education (PE) (3/4)',
+          semester: 3,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 0
+        },
+        {
+          title: 'Tourism and Environment',
+          semester: 3,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 3
+        },
+        {
+          title: 'Business Statistics II',
+          semester: 4,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 4
+        },
+        {
+          title: 'Entrepreneurship in Hospitality',
+          semester: 4,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 5
+        },
+        {
+          title: 'Fundamentals of Accounting II',
+          semester: 4,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 4
+        },
+        {
+          title: 'Hospitality Management (2/2)',
+          semester: 4,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 5
+        },
+        {
+          title: 'Marketing in Hospitality',
+          semester: 4,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 5
+        },
+        {
+          title: 'Physical Education (PE) (4/4)',
+          semester: 4,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 0
+        },
+        {
+          title: 'Business Finances (1/2)',
+          semester: 5,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 5
+        },
+        {
+          title: 'Evaluation of Cultural Heritage in Tourism',
+          semester: 5,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 3
+        },
+        {
+          title: 'Fundamentals of Management',
+          semester: 5,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 3
+        },
+        {
+          title: 'Fundamentals of Special Forms of Tourism',
+          semester: 5,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 3
+        },
+        {
+          title: 'International Trade',
+          semester: 5,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 3
+        },
+        {
+          title: 'Travel Agency Managment',
+          semester: 5,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 3
+        },
+        {
+          title: 'Business Finances (2/2)',
+          semester: 6,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 5
+        },
+        {
+          title: 'Final Thesis',
+          semester: 6,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 6
+        },
+        {
+          title: 'Practical Work',
+          semester: 6,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 7
+        },
+        {
+          title: 'Tourist Market Research',
+          semester: 6,
+          exam1: null,
+          exam2: null,
+          essay: null,
+          presentation: null,
+          ects: 5
+        },
         
       ]
     },
--- src/pages/GradesPage.jsx
+++ src/pages/GradesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import { UserContext } from "../context/UserContex";
 import ClassCard from "../components/Grades/ClassCard";
@@ -6,8 +6,13 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function GradesPage() {
   const { loggedInUser } = useContext(UserContext);
+  const [currentSemester, setCurrentSemester] = useState(1);
 
-  const displayClasses = loggedInUser.classes.map((classCard) => {
+  const filtered = loggedInUser.classes.filter((element) => {
+    return element.semester === currentSemester;
+  });
+
+  const displayClasses = filtered.map((classCard) => {
     return <ClassCard key={uuidv4()} classCard={classCard} />;
   });
 
@@ -15,9 +20,46 @@ export default function GradesPage() {
     <div id="grades">
       <Navbar />
       <div className="container">
-        <div className="grades-container">
-          {displayClasses}
+        <div className="semester-container">
+          <h4>Semesters:</h4>
+          <div
+            className={currentSemester === 1 ? "semester active-semester" : "semester"} 
+            onClick={() => {setCurrentSemester(1)}}
+          >
+           1
+            </div>
+          <div
+            className={currentSemester === 2 ? "semester active-semester" : "semester"}
+            onClick={() => {setCurrentSemester(2)}}
+          >
+            2
+            </div>
+          <div
+            className={currentSemester === 3 ? "semester active-semester" : "semester"} 
+            onClick={() => {setCurrentSemester(3)}}
+          >
+            3
+            </div>
+          <div
+              className={currentSemester === 4 ? "semester active-semester" : "semester"} 
+              onClick={() => {setCurrentSemester(4)}}
+          >
+            4
+            </div>
+          <div
+            className={currentSemester === 5 ? "semester active-semester" : "semester"}  
+            onClick={() => {setCurrentSemester(5)}}
+          >
+            5
           </div>
+          <div 
+            className={currentSemester === 6 ? "semester active-semester" : "semester"}
+            onClick={() => {setCurrentSemester(6)}}
+          >
+            6
+          </div>
+        </div>
+        <div className="grades-container">{displayClasses}</div>
       </div>
     </div>
   );
